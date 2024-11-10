e87f6bb8679e18ff68eeb85e7a567d99bfe672f6
Dave Simmons <dbsimmons64@outlook.com>
2024-11-10 15:13:44 +0000

    Added Dadbod for Database Tables

---
+++ lua/shortlyportly/plugins/dadbod.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		"tpope/vim-dadbod",
+	},
+	{
+		"kristijanhusak/vim-dadbod-completion",
+	},
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		init = function()
+			vim.g.db_ui_use_nerd_fonts = 1
+			vim.g.db_ui_save_location = "../"
+		end,
+	},
+}
